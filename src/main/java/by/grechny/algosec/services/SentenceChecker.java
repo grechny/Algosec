b221d6c4f1370c9d5e278647d6ed30fe7f659d03
Aliaksandr Hrechny <grechny@sysadmins.by>
2017-12-11 09:07:49 +0300

    Minor refactoring

--- src/main/java/by/grechny/algosec/services/SentenceChecker.java
+++ src/main/java/by/grechny/algosec/services/SentenceChecker.java
@@ -27,20 +27,19 @@ public class SentenceChecker {
     boolean result = false;
 
     for (int i = 1; i <= dictionaryService.getTheLongestWordLength() && i <= sentence.length(); i++) {
-      result = isValidSentence(sentence.substring(0, i), sentence.substring(i, sentence.length()));
+      String word = sentence.substring(0, i);
+      String newSentence = sentence.substring(i, sentence.length());
+
+      result = dictionaryService.existsInDictionary(word);
+      if (result && !StringUtils.isEmpty(newSentence)) {
+        result = isValidSentence(newSentence);
+      }
+
       if (result) {
         break;
       }
     }
 
-    return result;
-  }
-
-  private boolean isValidSentence(String word, String sentence) {
-    boolean result = dictionaryService.existsInDictionary(word);
-    if (result && !StringUtils.isEmpty(sentence)) {
-      result = isValidSentence(sentence);
-    }
     return result;
   }
 }
